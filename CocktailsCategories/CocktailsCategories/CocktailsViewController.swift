aa7361e50068c4d180fb78f4596f09c84df23f25
VadimSorokolit <macintosh@ukr.net>
2024-02-25 01:37:23 +0200

    Added button without storyboard

--- CocktailsCategories/CocktailsCategories/CocktailsViewController.swift
+++ CocktailsCategories/CocktailsCategories/CocktailsViewController.swift
@@ -14,10 +14,23 @@ class CocktailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .yellow
         self.title = "Good !!!"
+        self.myButton()
         cocktailsViewModel.getCoctailsCategory()
     }
-
+    
+    // MARK: Methods
+    
+    private func myButton() {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 160, height: 60))
+        button.center = view.center
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 12
+        button.setTitle("First button", for: .normal)
+        button.layer.masksToBounds = true
+        view.addSubview(button)
+    }
+    
 }
 
--- CocktailsCategories/CocktailsCategories/SceneDelegate.swift
+++ CocktailsCategories/CocktailsCategories/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = CocktailsViewController()
-        window?.rootViewController = UINavigationController(rootViewController: viewController)
+        let cocktailsViewController = CocktailsViewController()
+        window?.rootViewController = UINavigationController(rootViewController: cocktailsViewController)
         window?.makeKeyAndVisible()
     }
     
