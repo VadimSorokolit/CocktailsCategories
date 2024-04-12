8fcababcfaa3d9a517e7d594a67ab0c1d34dad1b
VadimSorokolit <macintosh@ukr.net>
2024-04-12 11:58:21 +0300

    Code refactoring

--- CocktailsCategories/CocktailsCategories/Classes/Views/Controllers/CocktailsViewController.swift
+++ CocktailsCategories/CocktailsCategories/Classes/Views/Controllers/CocktailsViewController.swift
@@ -232,6 +232,12 @@ extension CocktailsViewController: UITableViewDataSource {
     
 }
 
+extension CocktailsViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let tableView = scrollView as? UITableView else { return }
+    }
+}
 
 
 
